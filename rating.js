c4b5b88fb91f8739002c70d33d880a1a382fb267
Oshinowo Medinat <106126973+MedinatOshinowo@users.noreply.github.com>
2022-09-11 04:23:26 +0100

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,12 @@
+
+
+
+function redirect(){
+    if (document.getElementById("submit").onclick){
+        window.location.assign("ThanksCard.html")
+    }
+    else{
+        alert("Please rate us")
+    }
+}
+ 
